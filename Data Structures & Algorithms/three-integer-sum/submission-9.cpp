28db764638862af43557cdb06557b4c384304107
Laith Omar <142274982+LaithOmar04@users.noreply.github.com>
2026-08-24 21:21:56 -0700

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res; 
+        for(int i = 0; i < nums.size(); ++i) {
+            if(i != 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            int begin = i + 1;
+            int end = nums.size() - 1;
+            while(begin < end) {
+                int sum = nums[i] + nums[begin] + nums[end];
+                if(sum > 0) {
+                    --end;
+                } else if(sum < 0) {
+                    ++begin;
+                } else {
+                    res.push_back({nums[i], nums[begin], nums[end]});
+                    
+                    ++begin;
+                    while(begin < end && nums[begin] == nums[begin - 1]) {
+                        ++begin;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+};
